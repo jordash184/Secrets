e9fab0c9375d9d9a29cacdbc75e0224fd4d03e3b
jordash184 <47244556+jordash184@users.noreply.github.com>
2019-04-29 11:44:17 +0100

    Level 3 security implemented - MD5 hashed passwords

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
 const app = express();
+const md5 = require('md5');
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -20,10 +20,6 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt, {
-  secret:process.env.SECRET,
-  encryptedFields:["password"]
-});
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -41,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register",function(req, res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -54,7 +50,7 @@ app.post("/register",function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email:username}, function(err, foundUser){
     if(err){
       console.log(err);
